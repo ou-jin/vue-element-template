bd6c7d3a7112d88c5a6a6481e22eddb694bba232
oujin <945538629@qq.com>
2019-03-26 16:49:01 +0800

    api change

--- src/api/apiUtil.js
+++ src/api/apiUtil.js
@@ -1,106 +1,146 @@
+import 'babel-polyfill'
+import store from '../vuex/store'
 import axios from 'axios'
+import qs from 'qs'
 import lodash from 'lodash'
-import { Message } from 'element-ui'
+import {Message} from 'element-ui'
 import Router from '../router/index'
-import { Loading } from 'element-ui';
-const baseUrl = serverConfig.baseUrl;
-// 生成全屏loading示例
-let loadingInstance = {}
+
 const generateApiMap = (map) => {
     let facade = {}
     lodash.forEach(map, function (value, key) {
-        facade[key] = toMethod(value);
+        facade[key] = toMethod(value)
     })
-    return facade;
+    return facade
 }
 
 const toMethod = (options) => {
-    options.method = options.method || 'post';
-    return (params = {}, attachedParams, config = {}) => {
-        params = lodash.extend(params, attachedParams);
-        return sendApiInstance(options.method, options.url, Object.assign(params,{'OrganizationID':localStorage.getItem('OrganizationID')}), config)
+    options.method = options.method || 'post'
+    return (params = {}, config = {}) => {
+        // if (window.localStorage.getItem(options.flag?options.flag+'USER_INFO':'USER_INFO')) {
+        //     params.UserInfo = JSON.parse(window.localStorage.getItem(options.flag?options.flag+'USER_INFO':'USER_INFO'));
+        // }
+
+        // var newParams = {input: JSON.stringify(params)};
+
+        return sendApiInstance(options.method, options.url,options.host, params, config)
     }
 }
 
-const createApiInstance = (config = {}, method, url) => {
+// 创建axios实例
+const createApiInstance = (config = {}, method, url,host) => {
     var _config = {
         withCredentials: true, // 跨域
-        baseURL: baseUrl
-    }
+        baseURL: host?host:process.env.NODE_ENV == 'development' ? process.env.API_HOST:HOST,
+    };
     config = lodash.merge(_config, config);
+    console.log(config)
     return axios.create(config);
 }
 
+// 检查返回值
 const errCheck = (code, message, data) => {
-    // if (data.isSuccess) {
-    //     return true
-    // }
-    return true
-    // if (code == 200 && data.status == 0) {
-    //     return true
-    // }
+    if (code == 200 && data.isSuccess) {
+        return true
+    }
+    return false
 }
 
-const sendApiInstance = (method, url, params, config = {}) => {
+const sendApiInstance = (method, url,host, params, config = {}) => {
+    let flag = Router.history.current.name==='FreeAuthReport'||Router.history.current.params.auth_obj?'FREE_':''
+    if (!url) {
+        return
+    }
+    if (window.localStorage.getItem(flag?flag+'USER_INFO':'USER_INFO')) {
+        params.UserInfo = JSON.parse(window.localStorage.getItem(flag?flag+'USER_INFO':'USER_INFO'));
+    }
+    params =  {input: JSON.stringify(params)};
+    store.state.loading = true;
 
-    if (!url) return;
-    let instance = createApiInstance(config, method, url);
+    let instance = createApiInstance(config, method, url,host);
+
     instance.interceptors.request.use(
         config => {
-            // 在请求时打开loading
-            loadingInstance = Loading.service({ fullscreen: true });
-            config.headers.Authorization = localStorage.getItem('AUT_TOKEN');
+            // 不需要设置token的，设置为空
+            if (url.indexOf('gettoken') > -1 || url.indexOf('registeruser') > -1) {
+                config.headers.Authorization = "";
+            }
+            else { // 其他所有需要token的，从本地获取token
+                // config.headers.Authorization = localStorage.getItem("AUT_TOKEN");
+                config.headers.Authorization = localStorage.getItem(flag?flag+"AUT_TOKEN":"AUT_TOKEN");
+            }
             return config;
         },
         error => {
             return Promise.reject(error);
         }
-    )
+    );
 
     instance.interceptors.response.use(
         response => {
-             // 请求结束后关闭loading
-            loadingInstance.close();
-            let { status, statusText, data } = response;
+            store.state.loading = false;
+            let {status, statusText, data} = response;
             if (errCheck(status, statusText, data) && data) {
-                // 后台请求每次response都会包含一个新的token 刷新token防止浏览中途登录过期
-                if (data.token) localStorage.setItem('AUT_TOKEN', data.token)
-                return Promise.resolve(data);
+                if(data.newToken) localStorage.setItem(flag?flag+"AUT_TOKEN":"AUT_TOKEN", data.newToken)
+                return Promise.resolve(data)
             } else {
                 return Promise.reject(data).then().catch(res => {
-                    let options = { message: res.desc, duration: 1000 };
+                    var options = {
+                        message: res.message,
+                        center: true
+                    };
                     Message.error(options);
                 })
             }
-            //    return response
         },
         error => {
+            //  console.log('报错内容返回值',error.response, error.message);
+            store.state.loading = false;
             return Promise.reject(error).then().catch(res => {
                 if (error.response.status == 401) {
-                    // 如果登录时间过长 则检测超时状态码401 清空本地local存储 并且跳转回login页面
+                    /*
+                    * 如果登录时间过长 则检测超时状态码401
+                    * 清空本地local存储 并且跳转回login页面
+                    * */
                     Message.error({
                         message: '登录过期请重新登录',
+                        center: true,
                         showClose: true,
-                        duration: 1000,
+                        duration: 2000,
                         onClose: () => {
-                            localStorage.clear(); // 清空本地localStorage缓存
-                            sessionStorage.clear(); // 清空本地sessionStorage缓存
-                            Router.push({ 'path': '/login', query: { redirect: window.location.host } });
+                            localStorage.clear(); // 清空本地缓存
+                            // location.reload(); // 返回登录页时强制刷新
+                            Router.push({'path': '/login'});
                         }
                     });
-                } else {
-                    // 服务端报错 处理
-                    var options = { message: '服务端出错, 请稍后再试', duration: 1000 };
+                }
+                else {
+                    var options = {
+                        message: error.response.status + ':' + error.message,
+                        center: true
+                    };
                     Message.error(options);
                 }
+
             })
         }
-    )
+    );
 
-    if (method === 'get' || method === 'delete') {
+    if (method === 'get') {
         params = {
-            params: params,
+            params: params
         }
+    }
+    if (method === 'delete') {
+        params = {
+            params: params
+        }
+    }
+    if (method === 'post') {
+        params = qs.stringify(params);
+    }
+    if (method === 'put') {
+        params = qs.stringify(params);
     }
     return instance[method](url, params, config)
 }
